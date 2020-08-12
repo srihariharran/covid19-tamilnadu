703dc2573633847e9bba45b3300dd413102feb2c
Sri Hari Harran B <69569863+srihariharran@users.noreply.github.com>
2020-08-12 14:13:55 +0530

    Add files via upload

---
+++ precache-manifest.0c4833e0514044ac9fcd2455e06f94be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4393557ce00d887e66eaa249a40e49ca",
+    "url": "/srihariharran/covid19-tamilnadu/index.html"
+  },
+  {
+    "revision": "5d63ad345d74343f502f",
+    "url": "/srihariharran/covid19-tamilnadu/static/css/main.30e4ede9.chunk.css"
+  },
+  {
+    "revision": "89b36be643c37619829e",
+    "url": "/srihariharran/covid19-tamilnadu/static/js/2.dba35a83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/srihariharran/covid19-tamilnadu/static/js/2.dba35a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d63ad345d74343f502f",
+    "url": "/srihariharran/covid19-tamilnadu/static/js/main.81c42a53.chunk.js"
+  },
+  {
+    "revision": "4fcb5182377aa2304705",
+    "url": "/srihariharran/covid19-tamilnadu/static/js/runtime-main.0ceaffe9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/srihariharran/covid19-tamilnadu/static/media/logo.5d5d9eef.svg"
+  }
+]);
